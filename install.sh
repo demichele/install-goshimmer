dc332dd6c1e912584f0520207d9ec4bb08916ae7
Daniel De Michele <carpdemichele@gmail.com>
2020-07-06 23:00:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 # Run all commands needed to install GoShimmer
 apt update && apt dist-upgrade -y
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
@@ -25,8 +25,9 @@ cd grafana/provisioning/datasources
 wget https://raw.githubusercontent.com/demichele/install-goshimmer/master/datasources.yaml
 cd /opt/goshimmer/prometheus/grafana/dashboards
 wget https://raw.githubusercontent.com/demichele/install-goshimmer/master/dashboards.yaml
+cd /opt/goshimmer
 wget https://raw.githubusercontent.com/iotaledger/goshimmer/master/tools/monitoring/grafana/dashboards/local_dashboard.json
-cp local_dashboard.json /opt/goshimmer/prometheus/grafana/dashboards
+cp local_dashboard.json grafana/dashboards
 chmod -R 777 grafana
 docker-compose up -d
 exec bash
